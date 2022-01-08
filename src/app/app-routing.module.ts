cb3b3bc86be1493a2f5ad836e37a2a9a6eb933a0
Abhishikha Maurya <abhishikha.maurya1998@gmail.com>
2022-01-08 13:32:31 +0530

    [Abhishikha] initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from "./admin/dashboard/dashboard.component";
+import {AboutComponent} from "./admin/about/about.component";
+import {MyProfileComponent} from "./admin/my-profile/my-profile.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"dashboard", component:DashboardComponent},
+  {path:"about", component:AboutComponent},
+  {path:"my-profile", component:MyProfileComponent},
+  {path:"", redirectTo:"dashboard", pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
