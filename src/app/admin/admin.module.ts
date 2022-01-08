9cff2e8eae9ea2e28dcb3c6cb72957f0b8697178
Abhishikha Maurya <abhishikha.maurya1998@gmail.com>
2022-01-08 15:06:23 +0530

    [Abhishikha] added dashboard service

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
 import { AboutComponent } from './about/about.component';
+import {DashboardService} from "../dashboard.service";
 
 @NgModule({
   declarations: [DashboardComponent, MyProfileComponent, AboutComponent],
@@ -11,6 +12,6 @@ import { AboutComponent } from './about/about.component';
     CommonModule
   ],
   exports: [DashboardComponent, MyProfileComponent, AboutComponent],
-  providers: [],
+  providers: [DashboardService],
 })
 export class AdminModule { }
